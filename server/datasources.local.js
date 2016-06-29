d2e0852a7ca28a1fa4b8a5281ff6ecb710be5ec7
Abraham Panduro <panduroab@gmail.com>
2016-06-28 22:26:56 -0500

    ACL config and connection with MongoDB using mongodb connector

---
+++ server/datasources.local.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "db": {
+    "host"      : process.env.DB_HOST,
+    "port"      : process.env.DB_PORT,
+    "database"  : process.env.DB_NAME,
+    "username"  : process.env.DB_USER,
+    "password"  : process.env.DB_PASS,
+    "name"      : "db",
+    "connector" : "mongodb"
+  }	
+};
